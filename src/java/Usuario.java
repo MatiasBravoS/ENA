eaf66c36f61aa1b6be6cdb34939bf9a923d05848
Jibun <Jibun@DESKTOP-FOAJH1H>
2018-06-15 21:03:49 -0400

    Usuario

---
+++ src/java/Usuario.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jibun
+ */
+public class Usuario {
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    private String usuario;
+    private String password;
+    
+    
+}
